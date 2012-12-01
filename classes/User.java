5fa098dead47a83c7b3ca458876af15c1b018d7e
dluce <dluce@mail.umw.edu>
2012-12-01 04:50:33 -0500

    Preliminary Java Objects
    Very shakey versions of the objects we'll be using. I see what you mean
    about not really needing specific objects in a WebApp now, but I think
    we should hold on to them unless we manage to get an implementation
    going that compeltely excludes anything except queries and their
    results.
    
    These are 'just-in-case' classes for now.

---
+++ classes/User.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * @author David
+ */
+public class User {
+    
+    User(String n){
+        name = n;   
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    private char gender;
+    private String name;
+    
+}
